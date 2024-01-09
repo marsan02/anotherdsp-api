a070e4d8bdaef6af4b94800a9f7b0906111260fe
marsan02 <marcelrs@ucm.es>
2024-01-09 17:34:02 +0100

    Moved main app to application.py

--- application.py
+++ application.py
@@ -1,19 +1,12 @@
 # application.py
 
 from flask import Flask
-from utils.logger import setup_logging
-from services.campaign_selection_service import load_campaigns_into_cache 
-from bidbus.routes import configure_routes
+from main import configure_routes
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-
-    # Load campaigns into cache
-    with app.app_context():
-        load_campaigns_into_cache()
-
-    # Setup logging
-    setup_logging()
+    CORS(app)
 
     # Configure routes
     configure_routes(app)
@@ -21,3 +14,5 @@ def create_app():
     return app
 
 app = create_app()  # Rename 'application' to 'app'
+if __name__ == '__main__':
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -9,49 +9,50 @@ import os
 import services.campaign as campaigns
 load_dotenv()  # This loads the environment variables from .env
 
-app = Flask(__name__)
-CORS(app)
 
-# MongoDB Atlas connection URI
-mongo_uri = os.environ.get("MONGO_URI")
-client = MongoClient(mongo_uri)
-app.register_error_handler(AuthError, handle_auth_error)
 
-@app.route('/countries')
-@requires_auth
-def get_countries():
-    return geo_api.get_countries()
+def configure_routes(app):
 
-@app.route('/submit-campaign', methods=['POST'])
-@requires_auth
-def submit_campaign():
-    return campaigns.post_campaign(request,client)
 
-@app.route('/campaigns', methods=['GET'])
-@requires_auth
-def manage_campaigns():
-    return campaigns.list_all_campaigns(client)
+    # MongoDB Atlas connection URI
+    mongo_uri = os.environ.get("MONGO_URI")
+    client = MongoClient(mongo_uri)
+    app.register_error_handler(AuthError, handle_auth_error)
 
-@app.route('/campaigns/<campaign_name>', methods=['GET', 'PUT', 'DELETE'])
-@requires_auth
-def process_campaign(campaign_name):
-    if request.method == 'GET':
-        #retrieve campaign
-        return campaigns.get_campaign(campaign_name,client)
-    elif request.method == 'PUT':
-        # Update a campaign
-        return campaigns.put_campaign(campaign_name,request.json,client)
- 
-    elif request.method == 'DELETE':
-        # Delete a campaign
-        return campaigns.delete_campaign(campaign_name,client)
+    @app.route('/countries')
+    @requires_auth
+    def get_countries():
+        return geo_api.get_countries()
 
-# Example of a protected route
-@app.route('/your-public-route')
-@requires_auth
-def your_protected_route():
-    # Now this route only requires the user to be authenticated
-    return 'Access Granted'
+    @app.route('/submit-campaign', methods=['POST'])
+    @requires_auth
+    def submit_campaign():
+        return campaigns.post_campaign(request,client)
+
+    @app.route('/campaigns', methods=['GET'])
+    @requires_auth
+    def manage_campaigns():
+        return campaigns.list_all_campaigns(client)
+
+    @app.route('/campaigns/<campaign_name>', methods=['GET', 'PUT', 'DELETE'])
+    @requires_auth
+    def process_campaign(campaign_name):
+        if request.method == 'GET':
+            #retrieve campaign
+            return campaigns.get_campaign(campaign_name,client)
+        elif request.method == 'PUT':
+            # Update a campaign
+            return campaigns.put_campaign(campaign_name,request.json,client)
+    
+        elif request.method == 'DELETE':
+            # Delete a campaign
+            return campaigns.delete_campaign(campaign_name,client)
+
+    # Example of a protected route
+    @app.route('/')
+    def your_protected_route():
+        # Now this route only requires the user to be authenticated
+        return 'Welcome to Another DSP API'
 
 if __name__ == '__main__':
     app.run(debug=True)
